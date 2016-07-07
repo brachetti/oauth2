e18ef2850e4d8831503dbcbb7dde24021baff4db
Ronald Brachetti <ronald.brachetti@gmail.com>
2016-07-07 11:23:44 +0200

    farady error

--- lib/oauth2/client.rb
+++ lib/oauth2/client.rb
@@ -86,8 +86,7 @@ module OAuth2
     # @option opts [Symbol] :parse @see Response::initialize
     # @yield [req] The Faraday request
     def request(verb, url, opts = {}) # rubocop:disable CyclomaticComplexity, MethodLength, Metrics/AbcSize
-      logger = ::Logger.new($stdout)
-      connection.response :logger, logger if ENV['OAUTH_DEBUG'] == 'true'
+      connection.response :logger, ::Logger.new($stdout) if ENV['OAUTH_DEBUG'] == 'true'
 
       url = connection.build_url(url, opts[:params]).to_s
 
